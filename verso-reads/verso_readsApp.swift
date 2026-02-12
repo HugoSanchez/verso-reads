38cc72a5a0b59b1628f32178c9fe6e1d9b76c1a6
HugoSanchez <hugosanchez251@gmail.com>
2026-02-12 12:57:25 +0100

    Initial Commit

---
+++ verso-reads/verso_readsApp.swift
@@ -0,0 +1,17 @@
+//
+//  verso_readsApp.swift
+//  verso-reads
+//
+//  Created by Hugo Sanchez on 12/2/26.
+//
+
+import SwiftUI
+
+@main
+struct verso_readsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
